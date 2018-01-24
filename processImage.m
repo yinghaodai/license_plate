bec4b52451e74ae53a0a2a4d7ec9aba9fa35df4b
yinghaodai <yinghaodai@hotmail.com>
2018-01-24 10:00:25 +0100

    Add license plate correcting image processor

--- processImage.m
+++ processImage.m
@@ -35,7 +35,72 @@ function licensePlate = processImage(image)
 %     if size(checkFormat(plate), 2) == 0 && size(checkFormat(newPlate), 2) > 0
 %         plate = newPlate;
 %     end;
-    licensePlate = checkFormat(plate);
-    %licensePlate = plate;
+    licensePlate = '';
+    groups = strsplit(plate, '-');
+    lonelyGroup = 0;
+    if size(groups, 2) == 3
+        for i = 1:3
+            if size(groups{i}, 2) == 1
+                lonelyGroup = i;
+            end
+            if ~all(isPlateLetter(groups{i})) && ~all(isstrprop(groups{i}, 'digit'))
+                group = groups{i};
+                group(group == 'B') = '8';
+                if ~all(group == groups{i}) && all(isstrprop(group, 'digit'))
+                    groups{i} = group;
+                else
+                    group = groups{i};
+                end
+                group(group == '8') = 'B';
+                if ~all(group == groups{i}) && all(isPlateLetter(group))
+                    groups{i} = group;
+                else
+                    group = groups{i};
+                end
+                group(group == 'S') = '5';
+                if ~all(group == groups{i}) && all(isstrprop(group, 'digit'))
+                    groups{i} = group;
+                else
+                    group = groups{i};
+                end
+                group(group == '5') = 'S';
+                if ~all(group == groups{i}) && all(isPlateLetter(group))
+                    groups{i} = group;
+                else
+                    group = groups{i};
+                end
+                group(group == 'Z') = '2';
+                if ~all(group == groups{i}) && all(isstrprop(group, 'digit'))
+                    groups{i} = group;
+                else
+                    group = groups{i};
+                end
+                group(group == '2') = 'Z';
+                if ~all(group == groups{i}) && all(isPlateLetter(group))
+                    groups{i} = group;
+                end
+            end
+        end
+        plate = strjoin(groups, '-');
+        licensePlate = checkFormat2(plate);
+        if lonelyGroup > 0 && size(licensePlate, 2) == 0
+            switch groups{lonelyGroup}
+                case 'B'
+                    groups{lonelyGroup} = '8';
+                case '8'
+                    groups{lonelyGroup} = 'B';
+                case 'S'
+                    groups{lonelyGroup} = '5';
+                case '5'
+                    groups{lonelyGroup} = 'S';
+                case 'Z'
+                    groups{lonelyGroup} = '2';
+                case '2'
+                    groups{lonelyGroup} = 'Z';
+            end
+            licensePlate = checkFormat2(strjoin(groups, '-'));
+        end
+        %licensePlate = plate;
+    end
 end
     
